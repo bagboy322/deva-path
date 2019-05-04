996dc04b9bc39dc68d528cb806f5ac07b5a842c8
bagboy322 <48098097+bagboy322@users.noreply.github.com>
2019-05-04 15:52:00 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,9 +11,69 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+    @commands.guild_only()
+    async def roles(self, ctx):
+        """ Get all roles in current server """
+        allroles = ""
+
+        for num, role in enumerate(sorted(ctx.guild.roles, reverse=True), start=1):
+            allroles += f"[{str(num).zfill(2)}] {role.id}\t{role.name}\t[ Users: {len(role.members)} ]\r\n"
+
+        data = BytesIO(allroles.encode('utf-8'))
+        await ctx.send(content=f"Roles in **{ctx.guild.name}**", file=discord.File(data, filename=f"{default.timetext('Roles')}"))
+
+    @commands.command()
+    @commands.guild_only()
+    async def joinedat(self, ctx, *, user: discord.Member = None):
+        """ Check when a user joined the current server """
+        if user is None:
+            user = ctx.author
+
+        embed = discord.Embed(colour=user.top_role.colour.value)
+        embed.set_thumbnail(url=user.avatar_url)
+        embed.description = f'**{user}** joined **{ctx.guild.name}**\n{default.date(user.joined_at)}'
+        await ctx.send(embed=embed)
+
+    @commands.group()
+    @commands.guild_only()
+    async def server(self, ctx):
+        if ctx.invoked_subcommand is None:
+            findbots = sum(1 for member in ctx.guild.members if member.bot)
+
+            embed = discord.Embed()
+            embed.set_thumbnail(url=ctx.guild.icon_url)
+            embed.add_field(name="Server Name", value=ctx.guild.name, inline=True)
+            embed.add_field(name="Server ID", value=ctx.guild.id, inline=True)
+            embed.add_field(name="Members", value=ctx.guild.member_count, inline=True)
+            embed.add_field(name="Bots", value=findbots, inline=True)
+            embed.add_field(name="Owner", value=ctx.guild.owner, inline=True)
+            embed.add_field(name="Region", value=ctx.guild.region, inline=True)
+            embed.add_field(name="Created", value=default.date(ctx.guild.created_at), inline=True)
+            await ctx.send(content=f"ℹ information about **{ctx.guild.name}**", embed=embed)
+
+    @server.command(name="avatar", aliases=["icon"])
+    @commands.guild_only()
+    async def server_avatar(self, ctx):
+        """ Get the current server icon """
+        await ctx.send(f"Avatar of **{ctx.guild.name}**\n{ctx.guild.icon_url_as(size=1024)}")
+
+    @commands.command()
+    @commands.guild_only()
+    async def user(self, ctx, *, user: discord.Member = None):
+        """ Get user information """
+        if user is None:
+            user = ctx.author
+
+        embed = discord.Embed(colour=user.top_role.colour.value)
+        embed.set_thumbnail(url=user.avatar_url)
+
+        embed.add_field(name="Full name", value=user, inline=True)
+        embed.add_field(name="Nickname", value=user.nick if hasattr(user, "nick") else "None", inline=True)
+        embed.add_field(name="Account created", value=default.date(user.created_at), inline=True)
+        embed.add_field(name="Joined this server", value=default.date(user.joined_at), inline=True)    
+    
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
     if message.author == client.user:
         return
     if message.content.startswith('!help'):
@@ -39,6 +99,8 @@ async def on_message(message):
     if message.content.startswith('!THEBEAST'):
         msg = '$im Kagami Uchiha'.format(message)
         await client.send_message(message.channel, msg)
+        
+        
 
 
 
